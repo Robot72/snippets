23c4c67ab0f1c4a24a8007637ad9b4221cd38055
Robert Kuznetsov <robertkuznetsou@gmail.com>
2024-04-12 04:03:14 +0200

    Added styling for the main page

--- app/page.tsx
+++ app/page.tsx
@@ -9,19 +9,22 @@ export default async function Home() {
       <Link 
         key={snippet.id}
         href={`/snippets/${snippet.id}`}
-        className="flex justify-between items-center p-2 m-2 border border-gray-300 rounded hover:bg-gray-100"
+        className="flex justify-between items-center p-2 px-7 border border-gray-300 rounded-lg hover:bg-gray-100"
        >
         <div>{snippet.title}</div>
-        <div>View</div>
+        <div className="bg-blue-400 border p-2 border-rounded rounded-md text-cyan-50">View</div>
       </Link>
     )
   })
   return (
-    <>
-      <h1> All Snippets </h1>
-      <div className="flex gap-2">
-        <div>{renderedSnippets}</div>
+    <div>
+      <div className="flex flex-row justify-between items-center m-2 ">
+        <h1 className="text-xl font-bold"> All Snippets </h1>
+        <Link href="/snippets/new" className="border p-2 border-rounded bg-blue-500 text-cyan-50 rounded-lg">New</Link>
       </div>
-    </>
+      <div className="flex flex-col gap-2">
+        {renderedSnippets}
+      </div>
+    </div>
   );
 }

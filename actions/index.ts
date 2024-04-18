6189dfe487024f9f379d6e5eefd535acc975a5bd
Robert Kuznetsov <robertkuznetsou@gmail.com>
2024-04-18 11:36:39 +0200

    Error handling for Create Snippet Action

--- actions/index.ts
+++ actions/index.ts
@@ -24,42 +24,45 @@ export async function createSnippet(
   formState: {message: string}, 
   formData: FormData
 ) {
-  const title = formData.get('title');
-  const code = formData.get('code');
+  try {
+    const title = formData.get('title');
+    const code = formData.get('code');
 
-  if (!title || !code) {
-    formState.message = "Please fill out both fields";
+    if (!title || !code) {
+      formState.message = "Please fill out both fields";
+    }
+
+    if (typeof title !== 'string' || typeof code !== 'string') {
+      formState.message = "Invalid input";
+    }
+
+    if (title.length > 100) {
+      formState.message = "Title is too long";
+    }
+
+    if (title.length < 3) {
+      formState.message = "Title must me at least 3 characters long";
+    }
+
+    if (code.length < 10) {
+      formState.message = "Code must be at least 10 characters long";
+    }
+
+    if (formState.message && formState.message.length > 0) {
+      return formState;
+    }
+
+    // Create a new record in th edatabase
+    const snippet = await db.snippet.create({
+        data: {
+            title,
+            code
+        }
+    });
+    // console.log('Created new snippet:', snippet)
+  } catch (error: unknown) {
+    return error instanceof Error ? { message: error.message } : { message: 'An unknown error occurred' };
   }
 
-  if (typeof title !== 'string' || typeof code !== 'string') {
-    formState.message = "Invalid input";
-  }
-
-  if (title.length > 100) {
-    formState.message = "Title is too long";
-  }
-
-  if (title.length < 3) {
-    formState.message = "Title must me at least 3 characters long";
-  }
-
-  if (code.length < 10) {
-    formState.message = "Code must be at least 10 characters long";
-  }
-
-  if (formState.message && formState.message.length > 0) {
-    return formState;
-  }
-
-  // Create a new record in th edatabase
-  const snippet = await db.snippet.create({
-      data: {
-          title,
-          code
-      }
-  });
-  console.log('Created new snippet:', snippet)
-
-  // Redirect the user back ot the root
   redirect('/');
 }
